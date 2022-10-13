9d35a2ea81f40f414a16c6377c483ec018d8022c
Rafael Forero <rafael.forero.mt@usa.edu.co>
2022-10-12 19:39:40 -0500

    prueba de api en github

---
+++ repamc/src/main/java/com/usa/repamc/repamc/RepamcApplication.java
@@ -0,0 +1,13 @@
+package com.usa.repamc.repamc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepamcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepamcApplication.class, args);
+    }
+
+}
---
+++ repamc/src/test/java/com/usa/repamc/repamc/RepamcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usa.repamc.repamc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepamcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
